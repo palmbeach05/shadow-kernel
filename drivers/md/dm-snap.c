9decf899efbd5448a46cc1371a8316ae77a11a35
Mikulas Patocka <mpatocka@redhat.com>
2009-10-16 23:18:16 +0100

    dm snapshot: lock snapshot while supplying status
    This patch locks the snapshot when returning status.  It fixes a race
    when it could return an invalid number of free chunks if someone
    was simultaneously modifying it.
    
    Cc: stable@kernel.org
    Signed-off-by: Mikulas Patocka <mpatocka@redhat.com>
    Signed-off-by: Alasdair G Kergon <agk@redhat.com>

--- drivers/md/dm-snap.c
+++ drivers/md/dm-snap.c
@@ -1204,6 +1204,8 @@ static int snapshot_status(struct dm_target *ti, status_type_t type,
 	unsigned sz = 0;
 	struct dm_snapshot *snap = ti->private;
 
+	down_write(&snap->lock);
+
 	switch (type) {
 	case STATUSTYPE_INFO:
 
@@ -1243,6 +1245,8 @@ static int snapshot_status(struct dm_target *ti, status_type_t type,
 					  maxlen - sz);
 		break;
 	}
+
+	up_write(&snap->lock);
 
 	return 0;
 }
