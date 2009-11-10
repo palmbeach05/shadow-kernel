63270d31c38d5b482dce13121dbe84c722b51d5b
stephen hemminger <shemminger@vyatta.com>
2009-11-10 07:20:34 +0000

    netdev: fold name hash properly (v3)
    The full_name_hash function does not produce well distributed values in
    the lower bits, so most code uses hash_32() to fold it.  This is really
    a bug introduced when name hashing was added, back in 2.5 when I added
    name hashing.
    
    hash_32 is all that is needed since full_name_hash returns unsigned int
    which is only 32 bits on 64 bit platforms.
    
    Also, there is no point in using hash_32 on ifindex, because the is naturally
    sequential and usually well distributed.
    
    Signed-off-by: Stephen Hemminger <shemminger@vyatta.com>
    Signed-off-by: Eric Dumazet <eric.dumazet@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -79,6 +79,7 @@
 #include <linux/cpu.h>
 #include <linux/types.h>
 #include <linux/kernel.h>
+#include <linux/hash.h>
 #include <linux/sched.h>
 #include <linux/mutex.h>
 #include <linux/string.h>
@@ -204,7 +205,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(net_napi_complete);
 static inline struct hlist_head *dev_name_hash(struct net *net, const char *name)
 {
 	unsigned hash = full_name_hash(name, strnlen(name, IFNAMSIZ));
-	return &net->dev_name_head[hash & (NETDEV_HASHENTRIES - 1)];
+	return &net->dev_name_head[hash_32(hash, NETDEV_HASHBITS)];
 }
 
 static inline struct hlist_head *dev_index_hash(struct net *net, int ifindex)
