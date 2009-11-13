922168d7f483bb0a3a10b9a8633153a0300f39b7
Thomas Gleixner <tglx@linutronix.de>
2009-11-14 00:26:34 +0100

    clocksource/events: Fix fallout of generic code changes
    powerpc grew a new warning due to the type change of clockevent->mult.
    
    The architectures which use parts of the generic time keeping
    infrastructure tripped over my wrong assumption that
    clocksource_register is only used when GENERIC_TIME=y.
    
    I should have looked and also I should have known better. These
    renitent Gaul villages are racking my nerves. Some serious deprecating
    is due.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/time/clocksource.c
+++ kernel/time/clocksource.c
@@ -507,8 +507,6 @@ static u64 clocksource_max_deferment(struct clocksource *cs)
 	return max_nsecs - (max_nsecs >> 5);
 }
 
-#ifdef CONFIG_GENERIC_TIME
-
 /**
  * clocksource_max_deferment - Returns max time the clocksource can be deferred
  * @cs:         Pointer to clocksource
@@ -549,6 +547,8 @@ static u64 clocksource_max_deferment(struct clocksource *cs)
 	 */
 	return max_nsecs - (max_nsecs >> 5);
 }
+
+#ifdef CONFIG_GENERIC_TIME
 
 /**
  * clocksource_select - Select the best clocksource available
