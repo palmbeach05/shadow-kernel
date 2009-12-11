cdbe226d1d98fbae6713035c534a9fbde37a7d21
Li Zefan <lizf@cn.fujitsu.com>
2009-12-11 15:45:50 +0800

    tracing, slab: Fix no callsite ifndef CONFIG_KMEMTRACE
    For slab, if CONFIG_KMEMTRACE and CONFIG_DEBUG_SLAB are not set,
    __do_kmalloc() will not track callers:
    
     # ./perf record -f -a -R -e kmem:kmalloc
     ^C
     # ./perf trace
     ...
              perf-2204  [000]   147.376774: kmalloc: call_site=c0529d2d ...
              perf-2204  [000]   147.400997: kmalloc: call_site=c0529d2d ...
              Xorg-1461  [001]   147.405413: kmalloc: call_site=0 ...
              Xorg-1461  [001]   147.405609: kmalloc: call_site=0 ...
           konsole-1776  [001]   147.405786: kmalloc: call_site=0 ...
    
    Signed-off-by: Li Zefan <lizf@cn.fujitsu.com>
    Reviewed-by: Pekka Enberg <penberg@cs.helsinki.fi>
    Cc: Christoph Lameter <cl@linux-foundation.org>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: linux-mm@kvack.org <linux-mm@kvack.org>
    Cc: Eduard - Gabriel Munteanu <eduard.munteanu@linux360.ro>
    LKML-Reference: <4B21F8AE.6020804@cn.fujitsu.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- mm/slab.c
+++ mm/slab.c
@@ -3667,7 +3667,7 @@ __do_kmalloc_node(size_t size, gfp_t flags, int node, void *caller)
 	return ret;
 }
 
-#if defined(CONFIG_DEBUG_SLAB) || defined(CONFIG_KMEMTRACE)
+#if defined(CONFIG_DEBUG_SLAB) || defined(CONFIG_TRACING)
 void *__kmalloc_node(size_t size, gfp_t flags, int node)
 {
 	return __do_kmalloc_node(size, flags, node,
@@ -3687,7 +3687,7 @@ void *__kmalloc_node(size_t size, gfp_t flags, int node)
 	return __do_kmalloc_node(size, flags, node, NULL);
 }
 EXPORT_SYMBOL(__kmalloc_node);
-#endif /* CONFIG_DEBUG_SLAB */
+#endif /* CONFIG_DEBUG_SLAB || CONFIG_TRACING */
 #endif /* CONFIG_NUMA */
 
 /**
@@ -3719,7 +3719,7 @@ static __always_inline void *__do_kmalloc(size_t size, gfp_t flags,
 }
 
 
-#if defined(CONFIG_DEBUG_SLAB) || defined(CONFIG_KMEMTRACE)
+#if defined(CONFIG_DEBUG_SLAB) || defined(CONFIG_TRACING)
 void *__kmalloc(size_t size, gfp_t flags)
 {
 	return __do_kmalloc(size, flags, __builtin_return_address(0));
