7fab4ccf38b10ebef89f25ca7ba45d675e2ae8d7
Gustavo F. Padovan <padovan@profusion.mobi>
2010-03-05 13:42:58 -0800

    printk: avoid warning when CONFIG_PRINTK is disabled
    kernel/printk.c:72: warning: `saved_console_loglevel' defined but not used
    
    Signed-off-by: Gustavo F. Padovan <padovan@profusion.mobi>
    Cc: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/printk.c
+++ kernel/printk.c
@@ -77,8 +77,6 @@ int console_printk[4] = {
 };
 EXPORT_SYMBOL_GPL(console_printk);
 
-static int saved_console_loglevel = -1;
-
 /*
  * Low level drivers may need that to know if they can schedule in
  * their unblank() callback or not. So let's export it.
@@ -156,6 +154,7 @@ static char __log_buf[__LOG_BUF_LEN];
 static char *log_buf = __log_buf;
 static int log_buf_len = __LOG_BUF_LEN;
 static unsigned logged_chars; /* Number of chars produced since last read+clear operation */
+static int saved_console_loglevel = -1;
 
 #ifdef CONFIG_KEXEC
 /*
