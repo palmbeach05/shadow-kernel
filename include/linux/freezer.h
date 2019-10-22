1b46c128128ed1f98610e40341ff5f0f8e2fedc4
David <davmcleod87@aol.com>
2019-10-22 14:00:49 -0400

    Update freezer.h

--- include/linux/freezer.h
+++ include/linux/freezer.h
@@ -160,6 +160,17 @@ static inline void set_freezable_with_signal(void)
 	} while (try_to_freeze());					\
 	__retval;							\
 })
+
+#define wait_event_freezable_exclusive(wq, condition)			\
+({									\
+	int __retval;							\
+	do {								\
+		__retval = wait_event_interruptible_exclusive(wq, 	\
+				(condition) || freezing(current));	\
+	} while (try_to_freeze());					\
+	__retval;							\
+})
+
 #else /* !CONFIG_FREEZER */
 static inline int frozen(struct task_struct *p) { return 0; }
 static inline int freezing(struct task_struct *p) { return 0; }
